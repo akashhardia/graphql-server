b2d3f1d122a5e64fd4ba123ac84d7405d84991b3
Akash Hardia <aakashhardiya@gmail.com>
2019-07-11 19:28:42 +0530

    Refactor: Added separate folder for Resolvers

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import * as fs from 'fs';
 import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`${fs.readFileSync(__dirname.concat('/schema.graphql'))}`;
-
 const apollo = new ApolloServer({ typeDefs, resolvers });
 
 apollo.listen().then(({ url }) => {
